783f9dffbf5d4083a40ae5f9d5c9c55687edd111
Damien Dubost <ddubost6998@gmail.com>
2023-09-03 17:22:08 +0200

    Ajout de la fonction principal pour lancer une partie

--- _js/main.js
+++ _js/main.js
@@ -10,11 +10,40 @@ function getRandomWord() {
     return wordList[randomIndex];
 }
 
+function playGame(playerName) {
+    const maxAttempts = 10;
+    let attempts = 0;
+    const secretWord = getRandomWord();
+
+    alert(`C'est parti ${playerName} ! Le mot secret a 6 lettres. Commencez à deviner.`);
+
+    const guessForm = document.getElementById("guess-form");
+    guessForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const guessInput = document.getElementById("guess-input").value.toLowerCase();
+
+        if (attempts >= maxAttempts) {
+            alert(`Désolé, ${playerName}. Vous avez atteint le nombre maximum de tentatives. Le mot secret était "${secretWord}".`);
+            guessForm.removeEventListener("submit");
+        } else if (guessInput.length !== 6) {
+            alert("Le mot doit contenir exactement 6 lettres.");
+        } else {
+            const result = checkGuess(secretWord, guessInput);
+            updateGameBoard(result, guessInput);
+            attempts++;
+
+            if (result.wellplaced === 6) {
+                alert(`Félicitations, ${playerName} ! Vous avez trouvé le mot secret "${secretWord}" en ${attempts} tentatives.`);
+                guessForm.removeEventListener("submit");
+            }
+        }
+    });
+}
+
 window.addEventListener('DOMContentLoaded', function () {
     const playerName = getPlayerName();
     if (playerName) {
-        alert(`C'est parti ${playerName} !`);
-        // Vous pouvez ajouter ici le reste du code de votre jeu
+        playGame(playerName);
     } else {
         alert("Nom invalide. Rafraîchissez la page et entrez un nom valide.");
     }
